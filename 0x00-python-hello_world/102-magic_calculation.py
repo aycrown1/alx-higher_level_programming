9ada72162dd3baf94350501c76587108ab74d3c6
aycrown1 <ayoadeadedamola198@gmail>
2023-06-06 00:46:36 +0100

    ByteCode -> Python #1

---
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -0,0 +1,16 @@
+def magic_calculation(a, b):
+    """
+    Perform a magic calculation.
+
+    This function takes two numbers, `a` and `b`, and performs a magic calculation
+    by raising `a` to the power of `b` and adding 98 to the result.
+
+    Args:
+        a (int or float): The base number.
+        b (int or float): The exponent.
+
+    Returns:
+        int or float: The result of the magic calculation.
+    """
+    return 98 + (a ** b)
+    
