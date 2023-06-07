1d74fa9a7b160b16955c99ea4152f8d4af5d94f5
aycrown1 <ayoadeadedamola198@gmail>
2023-06-07 04:19:38 +0100

    a ^ b

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def pow(a, b):
+    """
+    Compute the value of a raised to the power of b.
+
+    Args:
+        a (int or float): The base value.
+        b (int or float): The exponent value.
+
+    Returns:
+        int or float: The result of a raised to the power of b.
+    """
+    result = a ** b
+    return result
