c96c2a7d2978097d8ad54f28753c1148058b1b24
aycrown1 <ayoadeadedamola198@gmail>
2023-06-07 04:19:06 +0100

    a + b

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def add(a, b):
+    """
+    Add two integers and return the result.
+
+    Args:
+        a (int): The first integer.
+        b (int): The second integer.
+
+    Returns:
+        int: The sum of the two integers.
+    """
+    return a + b
