3b9fe427f4cfd68d24198b18f89b382ff0cbf190
aycrown1 <ayoadeadedamola198@gmail>
2023-06-07 04:17:56 +0100

    To uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def uppercase(str):
+    """
+    Prints a string in uppercase
+    Args:
+        s (str): The string to be printed in uppercase.
+
+    Returns:
+        None
+    """
+    for c in str:
+        print("{}".format(chr(ord(c) - 32)), end='')
