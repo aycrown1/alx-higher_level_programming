162db361a4a97bc2a407bb8a6841eba69e0f1e95
aycrown1 <ayoadeadedamola198@gmail>
2023-06-08 19:44:21 +0100

    Import a simple function from a simple file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,15 +1,4 @@
 #!/usr/bin/python3
-"""
-This script imports the function 'add' from the file 'add_0.py'
-and prints the result of the addition a + b.
-
-a = 1
-b = 2
-
-The result is printed in the format:
-       '<a value> + <b value> = <add(a, b) value>'
-"""
-
 if __name__ == "__main__":
     from add_0 import add
 
