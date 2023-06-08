e1e043a4f79312d1dbd30f1490cf119b8afef139
aycrown1 <ayoadeadedamola198@gmail>
2023-06-08 19:46:00 +0100

    My first toolbox!

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,16 +1,4 @@
 #!/usr/bin/python3
-"""
-This script imports functions from the file 'calculator_1.py',
-performs mathematical operations using the imported functions,
-and prints the results.
-
-Variables:
-- a: assigned the value 10
-- b: assigned the value 5
-
-The results of addition, subtraction, multiplication, and division are printed.
-"""
-
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
 
