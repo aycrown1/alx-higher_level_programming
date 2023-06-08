67d4f6fa643927d18308f4fc3f5476b76ce30a33
aycrown1 <ayoadeadedamola198@gmail>
2023-06-08 18:54:39 +0100

    Infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,14 +3,15 @@ import sys
 
 if __name__ == "__main__":
     """
-    This script prints the result of the addition of all command-line arguments.
+    This script prints
+     the result of the addition of all command-line arguments.
 
     Usage: ./3-infinite_add.py arg1 arg2 ...
 
     Output format:
     <sum_of_arguments>
     """
- 
+
     result = 0
     for i in range(len(sys.argv) - 1):
         result += int(sys.argv[i + 1])
