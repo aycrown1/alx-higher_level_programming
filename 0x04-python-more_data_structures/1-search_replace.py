379aa9ec00a78e7efdf360c374d75f04cdadfa36
aycrown1 <ayoadeadedamola198@gmail>
2023-06-15 00:52:46 +0100

    Search and replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    """
+    Replaces all occurrences of an element with another element in a new list.
+
+    Args:
+        my_list (list): The initial list.
+        search: The element to replace in the list.
+        replace: The new element.
+
+    Returns:
+        list: A new list with all occurrences of the search element replaced by the replace element.
+    """
+    new_list = []
+    for element in my_list:
+        if element == search:
+            new_list.append(replace)
+        else:
+            new_list.append(element)
+
+    return new_list
