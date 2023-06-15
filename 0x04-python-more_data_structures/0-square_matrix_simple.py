b54bd845ded03324d5a7a06f769cfedb2716e298
aycrown1 <ayoadeadedamola198@gmail>
2023-06-15 01:43:21 +0100

    Squared simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
+    """
+    Compute the square value of all integers in a matrix.
+
+    Args:
+        matrix (list[list[int]]): The input matrix of integers.
+
+    Returns:
+        list[list[int]]: A new matrix with the same size as the input matrix.
+        Each value in the new matrix is the square of the corresponding value in the input matrix.
+        The input matrix remains unmodified.
+    """
+    # Create a new matrix with the same size as the input matrix
     result = list(map(lambda row: list(map(lambda x: x ** 2, row)), matrix))
-    
+
     return result
