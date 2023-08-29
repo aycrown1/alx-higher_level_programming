2ca4abd3b2cdb2d696ef63757ad94ad470b3e007
aycrown1 <ayoadeadedamola198@gmail.com>
2023-08-29 13:11:00 +0000

    Safe list printing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,16 +2,20 @@
 
 """
 Module: safe_print_list
-Description: This module contains a function to safely print elements from a list.
+Description:
+This module contains a function to safely print elements from a list.
 """
+
 
 def safe_print_list(my_list=[], x=0):
     """
     Safely prints the first 'x' elements from 'my_list'.
 
     Args:
-        my_list (list, optional): The list to print elements from. Defaults to an empty list.
-        x (int, optional): The number of elements to print. Defaults to 0.
+        my_list (list, optional):
+    The list to print elements from. Defaults to an empty list.
+        x (int, optional):
+    The number of elements to print. Defaults to 0.
 
     Returns:
         int: The real number of elements printed.
