26bef3f0367e73711043e99d7a042f64385255e2
aycrown1 <ayoadeadedamola198@gmail.>
2023-09-02 17:53:06 +0100

    Print and count integers

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,12 +4,13 @@ This module prints the first x elements of a list and only integers.
 func: safe_print_list_integers
 """
 
+
 def safe_print_list_integers(my_list=[], x=0):
     """
     prints the first x elements of a list and only integers
     args:
-    	my_list: a list that contain any type (integer, string, etc.)
-    	x: represents the number of elements to access in my_list
+    my_list: a list that contain any type (integer, string, etc.)
+    x: represents the number of elements to access in my_list
     return:  the real number of integers printed.
     """
 
