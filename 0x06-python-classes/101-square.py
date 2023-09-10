dd67967b2c03ac65749d07a8c2454b222d47426f
aycrown1 <ayoadeadedamola198@gmail.>
2023-09-10 20:01:05 +0100

    Print Square instance

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -111,25 +111,23 @@ class Square:
             raise ValueError("size must be >= 0")
         if self.__size == 0:
             print("")
-        else:
-            print('\n' * self.__position[1], end='')
-        for i in range(self.__size):
-            print(" " * self.__position[0], end='')
-            print("#" * self.__size)
+            return
+
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print("")
 
     def __str__(self):
         """
         Define the print() representation of a Square.
         """
         if self.__size != 0:
-            print("")
-            return
-
             [print("") for i in range(0, self.__position[1])]
             for i in range(0, self.__size):
                 [print(" ", end="") for j in range(0, self.__position[0])]
                 [print("#", end="") for k in range(0, self.__size)]
                 if i != self.__size - 1:
                     print("")
-            return("")
-        
+            return ("")
