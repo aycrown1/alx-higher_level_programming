daad23c6ea6b788fdfad2b9f32e396cd83f5e8f0
aycrown1 <ayoadeadedamola198@gmail.com>
2023-09-12 17:53:52 +0000

    semistandard --fix *js && chmod +x *js

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
   let count = 0;
-  for (let element in list) {
+  for (const element in list) {
     if (searchElement === list[element]) {
       count++;
     }
