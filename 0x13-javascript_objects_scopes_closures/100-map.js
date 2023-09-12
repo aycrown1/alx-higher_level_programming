5f490975ed5a27b0cfed2210e860fb0484d90ec1
ayoadeadedamola198@gmail.com <ayoadeadedamola198@gmail.com>
2023-09-12 18:43:35 +0100

    Factor index

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-let area = require('./100-data').list;
-let newArea = area.map(function (x, idx) {
-  return x * idx;
-});
-console.log(area);
-console.log(newArea);
+const list = require('./100-data.js').list;
+console.log(list);
+console.log(list.map((item, index) => item * index));
