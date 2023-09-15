fe15ee68f71f35b650cf3361ccf7fe03ae22e022
aycrown1 <ayoadeadedamola198@gmail.com>
2023-09-15 15:56:15 +0000

    Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,4 +1,4 @@
-#!/bin/usr/python3
+#!/usr/bin/python3
 
 """
 This module imports SQL into our python script.
@@ -8,14 +8,14 @@ The script lists all states with a name starting
 import sys
 import MySQLdb
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
 
-    db = MySQLdb.connect(host="localhost", user="username",
-                         passwd="password", db="database",
-                         port="3306")
+    db = MySQLdb.connect(host="localhost", user=username,
+                         passwd=password, db=database,
+                         port=3306)
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
     data = cursor.fetchall()
