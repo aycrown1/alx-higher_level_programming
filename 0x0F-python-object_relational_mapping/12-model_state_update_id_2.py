e28458a1586e0530dd29991dec8902be34236ad1
aycrown1 <ayoadeadedamola198@gmail.com>
2023-09-17 16:53:29 +0000

    pycodestyle *py checks

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-a script that changes the name of a State object from the database hbtn_0e_6_usa
+a script that changes the name of a State object from the database
 """
 if __name__ == '__main__':
     import sys
--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""A script takes in the name of a state as an argument and lists all cities of that
-   states using the hbtn_0e_4_usa database
+"""
+A script takes in the name of a state as an argument and lists all cities of
+  that states using the hbtn_0e_4_usa database
 """
 if __name__ == "__main__":
     import MySQLdb
@@ -8,7 +9,7 @@ if __name__ == "__main__":
     if len(sys.argv) == 5:
         s_list = []
         dbase = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                             passwd=sys.argv[2], db=sys.argv[3], port=3306)
+                                passwd=sys.argv[2], db=sys.argv[3], port=3306)
         cursor = dbase.cursor()
         cursor.execute("SELECT * FROM states,cities \
                     WHERE states.id=cities.state_id \
