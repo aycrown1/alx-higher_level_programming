82f8df0f8f45a5c1e36409aadcf739c805d287a7
aycrown1 <ayoadeadedamola198@gmail.com>
2023-09-29 14:27:52 +0000

    What's my status? #1

---
+++ 0x10-python-network_1/4-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Fetches https://alx-intranet.hbtn.io/status."""
+import requests
+
+
+if __name__ == "__main__":
+   url = requests.get("https://alx-intranet.hbtn.io/status")
+   try:
+       print("Body response:")
+       print("\t- type: {}".format(type(url.text)))
+       print("\t- content: {}".format(url.text))
+   except requests.exceptions.RequestException as e:
+       print(f"Request error: {}".format(e))
