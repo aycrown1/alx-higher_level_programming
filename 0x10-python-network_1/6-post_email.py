c29fea55885f47083c99d5cfe459387f32de4a11
aycrown1 <ayoadeadedamola198@gmail.com>
2023-09-29 14:54:46 +0000

    POST an email #1

---
+++ 0x10-python-network_1/6-post_email.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# sends a POST request to the passed URL
+from requests import post
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = {'email': sys.argv[2]}
+    response = post(url, email).content.decode('utf-8')
+    print(response.text)
