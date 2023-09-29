927e15fa19bc49f18a5fb8ea92d4d751e1ddc84a
aycrown1 <ayoadeadedamola198@gmail.com>
2023-09-29 14:59:06 +0000

    Error code #1

---
+++ 0x10-python-network_1/7-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# gets the status code to the passed URL
+from requests import get
+import sys
+
+if __name__ == "__main__":
+    try:
+        response = get(sys.argv[1])
+        if response.status_code >= 400:
+            print("Error code: {}".format(response.status_code))
+        else:
+            print("{}".format(response.content.decode('utf-8')))
+    except requests.exceptions.RequestException as e:
+        print(f"Request error: {e}")
